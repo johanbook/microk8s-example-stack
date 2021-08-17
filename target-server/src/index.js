550ed88c31b8c662775dcfb4339f5943073be7f4
Johan Book <johan.book@desupervised.io>
2021-08-17 21:33:46 +0200

    Intial commit

---
+++ target-server/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const logger = require("morgan");
+
+const PORT = 80;
+
+const app = express();
+app.use(logger("tiny"));
+app.use((req, res) => {
+  const userId = req.headers["x-user-id"];
+  res.send(`You have reached the target server ${userId}`);
+});
+app.listen(PORT);
